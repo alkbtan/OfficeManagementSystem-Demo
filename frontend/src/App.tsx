a635879261ca38c7a5042847c86e43a46fe1267f
alkbtan <mohammad93shaar@yahoo.com>
2026-07-03 01:31:01 +0300

    Initial React project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Office Management System</h1>
+      <p>Welcome to our system.</p>
+    </div>
+  )
+}
+
+export default App
